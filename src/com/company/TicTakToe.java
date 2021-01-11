3b2ec0f8c880c67517f971cddecba9b9ed22011a
vinay-ryakala <vinayrakyala@gmail.com>
2021-01-11 09:37:01 +0530

    ADDED: to create a new board

---
+++ src/com/company/TicTakToe.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class TicTakToe {
+    public static void main(String[] args) {
+        System.out.println("Welcome to TicTacToe game");
+        char[] board =createBoard();
+    }
+
+    private static char[] createBoard() {
+        char[] board = new char[10];
+        for (i = 1; i <= 10; i++)
+        {
+            board[i]=" ";
+        }
+    return borad;
+    }
+
+}
